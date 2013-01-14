037d89a4440970b53ec2ac9dabf9aa5fa37b9928
Devon Kearns <dookie@kali.org>
2013-01-14 07:18:47 -0700

    Pre-build commit

--- debian/enumiax.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the enumiax package
-#
-0 4	* * *	root	[ -x /usr/bin/enumiax_maintenance ] && /usr/bin/enumiax_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(enumiax):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="enumiax" command="/usr/bin/enumiax"
